a98516f048e21b600707fb0757abff31f1c80238
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2025-03-11 04:21:16 -0700

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerAdmin, login, getUserProfile } = require('../controllers/authController');
+const { registerAdmin, login, getUserProfile,getAllVolunteers } = require('../controllers/authController');
 const {protect} = require('../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.post('/register', registerAdmin);
 router.post('/login', login);
 router.get('/profile', protect, getUserProfile);
-
+router.get("/volunteers", getAllVolunteers);
 
 module.exports = router;
