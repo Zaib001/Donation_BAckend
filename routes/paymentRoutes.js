fcbdcde15c68b4e52ec97a9f094608e9f25a32fd
TalhaZaheer1 <tzaheer72@gmail.com>
2025-03-11 03:34:13 -0700

    razorchanges

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const { initiatePayment, verifyPayment, submitOfflinePayment } = require("../controllers/paymentController");
+const { submitOfflinePayment } = require("../controllers/paymentController");
 const { protect, adminOnly } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/initiate", protect, initiatePayment);
-router.post("/verify", protect, adminOnly, verifyPayment);
 router.post("/offline", protect, submitOfflinePayment);
 
 module.exports = router;
