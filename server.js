520665edde1e1a739d18a78684eaf92706ad8e9a
JahanZaib Malik <116706071+Zaib001@users.noreply.github.com>
2025-03-12 14:23:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://darulfalah.in", 
   methods: "GET,POST,PUT,DELETE",
   credentials: true
 }));
